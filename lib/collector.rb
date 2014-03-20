d3b3370ea4c3bad669baf9271b0675425655b577
wangxiangyu <wangxiangyu@baidu.com>
2014-03-20 17:24:07 +0800

    update

--- lib/collector.rb
+++ lib/collector.rb
@@ -17,6 +17,7 @@ module Collector
         end
         def run
             register_to_master
+            register_task
             sub_task
         end
         def get_ip
@@ -39,12 +40,20 @@ module Collector
             @sids = {}
         end
         def collect(message) 
-            range=message[:range]
-            range.each do |i|
-                sid=nats.subscribe("jpaas_collector_#{i}") do |message|
+            begin_index=message.data[:begin_index]
+            end_index=message.data[:end_index]
+            (begin_index..end_index).each do |i|
+                index=i/256
+                sid=nats.subscribe("jpaas_collector_#{index}") do |message|
                     process_message(message)
                 end
-                @sids["jpaas_collector_#{i}"]=sid
+                @sids[index]=sid
+            end
+        end
+        def register_task
+            EM::PeriodicTimer.new(2) do
+                index=@sids.keys
+                nats.publish("task_register",{:index=>index})
             end
         end
         def process_message(message)
