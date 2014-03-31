83b007ebcf14cad20145cebe2c13eba51bce4ee9
wangxiangyu <wangxiangyu@baidu.com>
2014-03-31 16:30:33 +0800

    update

--- lib/collector.rb
+++ lib/collector.rb
@@ -7,10 +7,13 @@ module Collector
     class Collector
         def initialize(config_path)
             @config=Config.new(config_path).config
-            @sids={}
+            @sids_dea_meta={}
+            @sids_instance_resource={}
+            @sids_instance_meta={}
             @nats=nil
             @local_ip=nil
-            @logger=Logger.new(config['logging']['file'])
+            #@logger=Logger.new(config['logging']['file'])
+            @logger=Logger.new(STDOUT)
             @logger.datetime_format = "%Y-%m-%d %H:%M:%S"
             @logger.formatter = proc do |severity, datetime, progname, msg|
                 "[#{datetime}] #{severity} : #{msg}\n"
@@ -51,29 +54,70 @@ module Collector
         end
         def clear_collect
             logger.debug("clear collector task")
-            @sids.each { |_, sid| nats.unsubscribe(sid) }
-            @sids = {}
+            @sids_dea_meta.each { |_, sid| nats.unsubscribe(sid) }
+            @sids_dea_meta={}
+            @sids_instance_resource.each { |_, sid| nats.unsubscribe(sid) }
+            @sids_instance_resource={}
+            @sids_instance_meta.each { |_, sid| nats.unsubscribe(sid) }
+            @sids_instance_meta={}
         end
         def collect(message) 
             start_index=message.data["start_index"]
             end_index=message.data["end_index"]
+            collect_instance_meta(start_index,end_index)
+            collect_instance_resource(start_index,end_index)
+            collect_dea_meta(start_index,end_index)
+        end
+
+        def collect_instance_meta(start_index,end_index)
             (start_index..end_index).each do |i|
                 index=i%256
-                logger.debug("sub task #{index}")
-                sid=nats.subscribe("dea.#{index}.snapshot") do |message|
-                    process_message(message)
+                logger.debug("sub instance meta #{index}")
+                sid=nats.subscribe("dea.#{index}.metadata") do |message|
+                    process_instance_meta(message)
                 end
-                @sids[index]=sid
+                @sids_instance_meta[index]=sid
             end
         end
+
+        def collect_instance_resource(start_index,end_index)
+            (start_index..end_index).each do |i|
+                index=i%256
+                logger.debug("sub instance_resource #{index}")
+                sid=nats.subscribe("dea.#{index}.instance.resource") do |message|
+                    process_instance_resource(message)
+                end
+                @sids_instance_resource[index]=sid
+            end
+        end
+
+        def collect_dea_meta(start_index,end_index)
+            (start_index..end_index).each do |i|
+                index=i%256
+                logger.debug("sub dea meta #{index}")
+                sid=nats.subscribe("dea.#{index}.nodedata") do |message|
+                    process_dea_meta(message)
+                end
+                @sids_dea_meta[index]=sid
+            end
+        end
+
         def register_task
             EM::PeriodicTimer.new(2) do
-                index=@sids.keys
+                index=@sids_dea_meta.keys
                 logger.debug("task register #{index}")
                 nats.publish("task_register",{:index=>index})
             end
         end
-        def process_message(message)
+        def process_instance_meta(message)
+            p message
+        end
+        
+        def process_instance_resource(message)
+            p message
+        end
+
+        def process_dea_meta(message) 
             p message
         end
     end
